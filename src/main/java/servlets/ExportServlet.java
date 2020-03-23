a15e83357b5a5b920e9097c6808e2de9ed89ebfa
mitilyas <mitilyas@mail.ru>
2020-03-23 11:19:05 +0400

    fix export

--- src/main/java/servlets/ExportServlet.java
+++ src/main/java/servlets/ExportServlet.java
@@ -5,19 +5,48 @@ import model.AdminMarketModel;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import javax.xml.bind.JAXBException;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.rmi.server.ExportException;
 
 @WebServlet("/export")
 public class ExportServlet extends HttpServlet {
     @EJB(beanName = "DBAdminMarketModel")
     private AdminMarketModel model;
+
+   /* @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String fileName = request.getParameter("file");
+        File file = new File(fileName);
+        ServletOutputStream outputStream = null;
+        BufferedInputStream inputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+            response.setContentLength((int) file.length());
+            FileInputStream fileInputStream = new FileInputStream(file);
+            inputStream = new BufferedInputStream(fileInputStream);
+            int readBytes = 0;
+            while ((readBytes = inputStream.read()) != -1)
+                outputStream.write(readBytes);
+        }catch (ExportException e){
+            e.printStackTrace();
+        }finally {
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+        }
+    }*/
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -36,9 +65,30 @@ public class ExportServlet extends HttpServlet {
                 break;
         }
         try {
-            model.toXmlFile(shopContent, "C:\\Users\\aaa\\IdeaProjects\\IronMarket\\src\\main\\resources\\1.xml");
+            model.toXmlFile(shopContent, filePath);
         } catch (JAXBException e) {
             e.printStackTrace();
+        }
+
+        File file = new File(filePath);
+        ServletOutputStream outputStream = null;
+        BufferedInputStream inputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("Content-Disposition", "attachment; filename=\"" + filePath + "\"");
+            response.setContentLength((int) file.length());
+            FileInputStream fileInputStream = new FileInputStream(file);
+            inputStream = new BufferedInputStream(fileInputStream);
+            int readBytes = 0;
+            while ((readBytes = inputStream.read()) != -1)
+                outputStream.write(readBytes);
+        }catch (ExportException e){
+            e.printStackTrace();
+        }finally {
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
         }
         response.sendRedirect(request.getContextPath()+"/admin");
     }
